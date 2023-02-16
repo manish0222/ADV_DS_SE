39c362788a535c5d7df679c2d8251006e16ae91c
Manish Godbole <116292202+manish0222@users.noreply.github.com>
2023-02-16 10:44:33 +0530

    Asgn 1 changes

---
+++ BinarySearchTree.cpp
@@ -0,0 +1,82 @@
+//============================================================================
+// Name        : BinarySearchTree.cpp
+// Author      : manish
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+class node{
+public:
+	int no;
+	node* left,*right;
+	node(int no){
+		left=right=NULL;
+		this->no=no;
+	}
+};
+class BST{
+public:
+	node* root;
+	void insert(int x){
+		node* nn=new node(x);
+		node* curr=root,* parent;
+		if(root==NULL){
+			root=nn;
+		}
+		else{
+			while(true){
+				parent=curr;
+				if(x < parent->no){
+					curr=curr->left;
+					if(curr==NULL){
+						parent->left = nn;
+						return;
+					}
+				}
+				else{
+					curr=curr->right;
+					if(curr== NULL){
+						parent->right=nn;
+						return;
+					}
+				}
+			}
+		}
+	}
+
+	void InOrder(node* root){
+		node* curr=root;
+		if(curr==NULL) return;
+		InOrder(curr->left);
+		cout<<curr->no<<" ";
+		InOrder(curr->right);
+	}
+
+	void preOrder(node* root){
+		node* curr=root;
+		if(curr==NULL) return;
+		cout<<curr->no<<" ";
+		preOrder(curr->left);
+		preOrder(curr->right);
+	}
+	void search(int x){
+
+	}
+};
+
+int main() {
+	int u=6,k=0;
+	BST bst;
+	while(k<u){
+		cout<<"enter ";
+		int x;cin>>x;
+		bst.insert(x);
+		k++;
+	}k=0;
+	bst.InOrder(bst.root);cout<<endl;
+	bst.preOrder(bst.root);cout<<endl;
+	return 0;
+}
