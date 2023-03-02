3fb498f58c291a8d537959ca8eabf63c2e2fa920
Manish Godbole <116292202+manish0222@users.noreply.github.com>
2023-03-02 10:34:42 +0530

    non recur inorder,preorder

--- 2nd_BinaryTree.cpp
+++ 2nd_BinaryTree.cpp
@@ -1,3 +1,4 @@
+
 // Online C++ compiler to run C++ program online
 #include <iostream>
 #include <queue>
@@ -12,7 +13,7 @@ public:
         this->data=data;
         left=right=NULL;
     }
-    
+
     friend class BinaryTree;
 };
 class BinaryTree{
@@ -35,28 +36,48 @@ public:
       root->right=insert(root->right);
       return root;
   }
-  
+
   void inorderNonRecur(node* root){
       stack <node*> s;
       node* t=root;
-      bool isEmpty=false;
-      while(true){
+      while(t!=NULL || !s.empty()){
           while(t!=NULL){
               s.push(t);
               t=t->left;
-              cout<<t->data<<" ";
           }
           if(s.empty()){
-              isEmpty=true;;
               return;
           }
-          node* u=s.top();
+          t=s.top();
           s.pop();
-          cout<<u->data<<" ";
+          cout<<t->data<<" ";
           t=t->right;
-          if(isEmpty){return;}
       }
-      
+
+  }
+  void preorederNonRecur(node* root){
+	  stack<node*> s;
+	  if(root==NULL){cout<<"empty node \n";return;}
+	  s.push(root);
+	  node* t=NULL;
+	  while(!s.empty()){
+		  t=s.top();
+		  s.pop();
+		  cout<<t->data<<" ";
+		  if(t->left){s.push(t->left);}
+		  if(t->right){s.push(t->right);}
+	  }
+  }
+  void postOrderNonrecur(node* root){
+	  if(root==NULL){
+		  cout<<"empty ";
+		  return;
+	  }
+	  stack<node*> s;
+	  s.push(root);
+	  while(!s.empty()){
+
+	  }
   }
   void inorder(node* root){
       if(root==NULL){return;}
@@ -64,21 +85,21 @@ public:
       cout<<root->data<<" ";
       inorder(root->right);
   }
-  
+
   void preorder(node* root){
       if(root==NULL){return;}
       cout<<root->data<<" ";
       preorder(root->left);
       preorder(root->right);
   }
-  
+
   void postorder(node* root){
       if(root==NULL){return;}
       postorder(root->left);
       postorder(root->right);
       cout<<root->data<<" ";
   }
-  
+
   int Height(node* root){
       if(root == NULL){
           return 0;
@@ -88,7 +109,7 @@ public:
       int k=max(lval,rval)+1;
       return k;
   }
-  
+
   void insertLevelWise(node* &root){
       queue<node*> q;
       cout<<"enter data for root ";
@@ -119,7 +140,7 @@ public:
       return 1+inCount(root->left)+inCount(root->right);
       //T.C = O(n)
   }
-  
+
   int leafCount(node* root){
       if(!root->left && !root->right){
           return 1;
@@ -129,7 +150,7 @@ public:
       if(root->right){j=leafCount(root->right);}
       return (k+j);
   }
-  
+
   int intNode(node* root){
       int k=intNode(root->left);
       int j=intNode(root->right);
@@ -137,14 +158,14 @@ public:
           return 1;
       }
   }
-  
+
   void swapBT(node* &root){
       if(root==NULL)return ;
       swap(root->left,root->right);
       swapBT(root->left);
       swapBT(root->right);
   }
-  
+
   node* deleteNode(node* root){
       if(root==NULL){
           return NULL;
@@ -179,8 +200,8 @@ int main() {
     bt.insertLevelWise(bt.root);
     // bt.root=bt.insert(bt.root);
     while(true){
-        cout<<"enter\n0)enter data\n1)display\n2)swap tree\n3)find height\n"
-        "4)copy tree\n5)count all nodes\n6)delete nodes\n7)end\nchoice ";
+        cout<<"enter\n0)enter data\n1)display\n2)swap tree\n3)find height\n" ;
+        //"4)copy tree\n5)count all nodes\n6)delete nodes\n7)end\nchoice ";
         int n;cin>>n;
         if(n==0){
                 bt.insertLevelWise(bt.root);
@@ -190,6 +211,8 @@ int main() {
             cout<<"inorder ";bt.inorder(bt.root);cout<<"\n";
             cout<<"inorder non-recursively ";bt.inorderNonRecur(bt.root);cout<<"\n";
             cout<<"preorder ";bt.preorder(bt.root);cout<<"\n";
+            cout<<"preoreder NonRecur ";bt.preorederNonRecur(bt.root);cout<<"\n";
+            cout<<"postorder ";bt.postorder(bt.root);cout<<"\n";
             cout<<"postorder ";bt.postorder(bt.root);cout<<"\n";
             cout<<"-----------------------------------------\n";
         }
@@ -198,25 +221,30 @@ int main() {
             cout<<"---Swapped----\n";
         }
         else if(n==3){
-            cout<<"height "<<bt.Height(bt.root);cout<<"\n";
+            cout<<"\n-----------------------------------------\n";
+            cout<<"\t height of tree is "<<bt.Height(bt.root);cout<<"\n";
+            cout<<"-----------------------------------------\n";
+
         }
-        else if(n==4){
-            
-        }
+//        else if(n==4){
+//
+//        }
         else if(n==5){
+            cout<<"\n-----------------------------------------\n";
             cout<<"leafnode ";
             if(!bt.root){ cout<<"empty";cout<<"\n";}
             else{cout<<(bt.leafCount(bt.root));cout<<"\n";}
             cout<<"internalnode "<<bt.inCount(bt.root);cout<<"\n";
+            cout<<"-----------------------------------------\n";
         }
-        else if(n==6){
-            bt.root=bt.deleteNode(bt.root);
-        }else if(n==7){
-            cout<<"_________Ended_________\n";
-        }
+//        else if(n==6){
+//            bt.root=bt.deleteNode(bt.root);
+//        }else if(n==7){
+//            cout<<"_________Ended_________\n";
+//        }
     }
-    
-    //bt.levelTrav(bt.root);
+
+    //bt.levelTrav(bt.root);  to display all data in that level
 
     // cout<<"inorder ";bt.inorder(bt.root);cout<<"\n";
     //     cout<<"preorder ";bt.preorder(bt.root);cout<<"\n";
@@ -224,6 +252,8 @@ int main() {
     //  BinaryTree test;
     //  test=bt;
     // cout<<"inorder ";test.inorder(test.root);cout<<"\n";
-    
+
     return 0;
 }
+
+
